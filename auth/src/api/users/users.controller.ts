033f1680f9921c5ec700f32bd3faa638f44f8892
Atiwat Seenark <um_oom@hotmail.com>
2022-07-18 23:59:09 +0700

    add skaffold

---
+++ auth/src/api/users/users.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller("api/users")
+export class UsersController {
+  @Get("/")
+  getUsers() {
+    return "Hi there";
+  }
+}
--- auth/src/app.controller.ts
+++ auth/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/health")
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("test")
+  getTest() {
+    return "test";
   }
 }
